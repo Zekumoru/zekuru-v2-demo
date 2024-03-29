488d89e7efc29f79f3a87138bd2ac14ce0ea002c
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-29 13:27:23 +0100

    Put cooldowns on commands

--- app.ts
+++ app.ts
@@ -18,6 +18,7 @@ const token = process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,4 +1,4 @@
-import { CacheType, Events, Interaction } from 'discord.js';
+import { CacheType, Collection, Events, Interaction } from 'discord.js';
 import { DiscordEvent } from '../types/DiscordEvent';
 
 export default {
@@ -14,6 +14,34 @@ export default {
       );
       return;
     }
+
+    const { cooldowns } = interaction.client;
+
+    if (!cooldowns.has(command.data.name)) {
+      cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 3;
+    const cooldownAmount =
+      (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+    if (timestamps?.has(interaction.user.id)) {
+      const expirationTime =
+        (timestamps.get(interaction.user.id) ?? 0) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const expiredTimestamp = Math.round(expirationTime / 1000);
+        return interaction.reply({
+          content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+          ephemeral: true,
+        });
+      }
+    }
+
+    timestamps?.set(interaction.user.id, now);
+    setTimeout(() => timestamps?.delete(interaction.user.id), cooldownAmount);
 
     try {
       await command.execute(interaction);
