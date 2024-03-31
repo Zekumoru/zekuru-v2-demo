885b5a2e32ef0ae6ede7059df33b665c0de7983f
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-31 11:31:25 +0200

    Add image to wut-de-shewi command

--- commands/fun/wut-de-shewi.ts
+++ commands/fun/wut-de-shewi.ts
@@ -6,11 +6,12 @@ const data = new SlashCommandBuilder()
   .setDescription('Ehhhhhh?!?!!');
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
-  await interaction.reply('# Wut De Shewiiiiiiiiiiii!!');
+  await interaction.reply('# Wut De Shewiiiiiiiiiiii!!')
+  await interaction.channel?.send('https://cdn.discordapp.com/attachments/987752925688389675/1223925077377028106/Wut_Dehellpng.png?ex=661ba00d&is=66092b0d&hm=a563b63e8e1d2d69edcbc4c39c7a1d7b1db64c7de9ab846d092606bd1759a648&')
 };
 
 export default createCommand({
-  cooldown: 5,
+  cooldown: 0,
   data,
   execute,
 });
