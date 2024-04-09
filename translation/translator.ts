3440b7c1942efa3f0782288344aa614a27285192
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-09 21:09:50 +0200

    Load target and source languages

--- translation/translator.ts
+++ translation/translator.ts
@@ -3,4 +3,20 @@ import * as deepl from 'deepl-node';
 const deeplApiKey = process.env.DEEPL_API_KEY;
 const translator = new deepl.Translator(deeplApiKey ?? '');
 
+export const targetLanguages: deepl.Language[] = [];
+const loadTargetLanguages = async () => {
+  targetLanguages.push(...(await translator.getTargetLanguages()));
+  console.log('Target languages loaded.');
+};
+
+export const sourceLanguages: deepl.Language[] = [];
+const loadSourceLanguages = async () => {
+  sourceLanguages.push(...(await translator.getSourceLanguages()));
+  console.log('Source languages loaded.');
+};
+
+(async () => {
+  await Promise.all([loadTargetLanguages(), loadSourceLanguages()]);
+})();
+
 export default translator;
