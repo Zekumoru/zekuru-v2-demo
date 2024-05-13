bc1ed9dd62b50bdb7a370894cbf77393c54784cc
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-13 13:30:30 +0200

    Remove db logic from keyv

--- db/keyv.ts
+++ db/keyv.ts
@@ -1,21 +1,7 @@
 import Keyv from 'keyv';
-import * as deepl from 'deepl-node';
-
-type TChannelId = string;
-
-interface ITranslateChannel {
-  sourceLang: deepl.SourceLanguageCode;
-  targetLang: deepl.TargetLanguageCode;
-}
 
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 const keyv = {
-  guilds: new Keyv<ITranslateChannel>(MONGODB_CONNECTION_STRING, {
-    namespace: 'guilds',
-  }),
-  links: new Keyv<Map<TChannelId, TChannelId[]>>(MONGODB_CONNECTION_STRING, {
-    namespace: 'links',
-  }),
   testing: new Keyv<string>(MONGODB_CONNECTION_STRING, {
     namespace: 'testing',
   }),
