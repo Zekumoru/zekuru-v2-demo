c486acfecb7d660a94f729b6c8f7933824af2512
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-18 00:06:56 +0200

    Implement deleting message

--- utils/events/getMessagesFromMessageLink.ts
+++ utils/events/getMessagesFromMessageLink.ts
@@ -6,19 +6,23 @@ const getMessagesFromMessageLink = async (
   ignoreMessageId: string,
   guild: Guild
 ) => {
-  const messages = (await Promise.all(
-    messageLink.links.map(async (link) => {
-      // ignore the message that the user has reacted to
-      if (link.messageId === ignoreMessageId) return;
+  try {
+    const messages = (await Promise.all(
+      messageLink.links.map(async (link) => {
+        // ignore the message that the user has reacted to
+        if (link.messageId === ignoreMessageId) return;
 
-      const channel = guild.channels.cache.get(link.channelId);
-      if (!channel) return;
-      if (channel.type !== ChannelType.GuildText) return;
-      return await channel.messages.fetch(link.messageId);
-    })
-  )) as unknown as (Message<true> | undefined)[] | undefined;
+        const channel = guild.channels.cache.get(link.channelId);
+        if (!channel) return;
+        if (channel.type !== ChannelType.GuildText) return;
+        return await channel.messages.fetch(link.messageId);
+      })
+    )) as unknown as (Message<true> | undefined)[] | undefined;
 
-  return messages;
+    return messages;
+  } catch (error) {
+    return;
+  }
 };
 
 export default getMessagesFromMessageLink;
