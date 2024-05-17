5b958d549b3250c598046baac188703358210f03
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-18 01:59:13 +0200

    Add warning on signing out

--- commands/translation/sign-out.ts
+++ commands/translation/sign-out.ts
@@ -1,10 +1,14 @@
 import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
   ChatInputCommandInteraction,
   PermissionFlagsBits,
   SlashCommandBuilder,
 } from 'discord.js';
 import { createCommand } from '../../types/DiscordCommand';
 import translatorCache from '../../cache/translatorCache';
+import { updateTranslateMessages } from '../../events/messageUpdateTranslate';
 
 const data = new SlashCommandBuilder()
   .setName('sign-out')
@@ -30,10 +34,51 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
     return;
   }
 
-  await translatorCache.unset(interaction.guildId);
-  await interaction.reply({
-    content: `Bot has successfully signed out.`,
+  // confirm signing out
+  const confirm = new ButtonBuilder()
+    .setCustomId('confirm')
+    .setLabel('Yes, sign out')
+    .setStyle(ButtonStyle.Danger);
+
+  const cancel = new ButtonBuilder()
+    .setCustomId('cancel')
+    .setLabel('No')
+    .setStyle(ButtonStyle.Secondary);
+
+  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    cancel,
+    confirm
+  );
+
+  const response = await interaction.reply({
+    content: `Are you sure to sign out the bot? This will stop translating any further messages.`,
+    components: [row],
   });
+
+  try {
+    const confirmation = await response.awaitMessageComponent({
+      filter: (i) => i.user.id === interaction.user.id,
+      time: 30_000,
+    });
+
+    if (confirmation.customId === 'confirm') {
+      await translatorCache.unset(interaction.guildId);
+      await confirmation.update({
+        content: `You have successfully signed out the bot.`,
+        components: [],
+      });
+    } else if (confirmation.customId === 'cancel') {
+      await confirmation.update({
+        content: `Signing out has been cancelled.`,
+        components: [],
+      });
+    }
+  } catch (error) {
+    await interaction.editReply({
+      content: `Confirmation not received within 30 seconds, cancelling.`,
+      components: [],
+    });
+  }
 };
 
 export default createCommand({
